396d5cab3532c7875299a378e48c41759d383e0f
Matt Brigida <matt@complete-markets.com>
2017-10-11 15:14:33 -0400

    renamed general query because only queries one sch

--- querying_data/schedule_RI_query.R
+++ querying_data/schedule_RI_query.R
@@ -1,11 +1,12 @@
-## function to query net interest income ------
 library(xts)
 
 general_query <- function(ID, code){
-    ## this is tier 1 capital
-    ## looks like tier 1 capital is in RCRI from 03312015 forward.  Schedule RCRI doesn't exist before then.
+
+  ## this function can query any variable on the schedule RI -----
+  ## arguments to the function are ID (the firm's IDRSSD) and the variable code (starting wtih RD...)
 
     reports <- c("03312012/sch_RI_03312012.rds", "06302012/sch_RI_06302012.rds", "09302012/sch_RI_09302012.rds", "12312012/sch_RI_12312012.rds", "03312013/sch_RI_03312013.rds", "06302013/sch_RI_06302013.rds", "09302013/sch_RI_09302013.rds", "12312013/sch_RI_12312013.rds", "03312014/sch_RI_03312014.rds", "06302014/sch_RI_06302014.rds", "09302014/sch_RI_09302014.rds", "12312014/sch_RI_12312014.rds", "03312015/sch_RI_03312015.rds", "06302015/sch_RI_06302015.rds", "09302015/sch_RI_09302015.rds", "12312015/sch_RI_12312015.rds", "03312016/sch_RI_03312016.rds", "06302016/sch_RI_06302016.rds", "09302016/sch_RI_09302016.rds", "12312016/sch_RI_12312016.rds", "03312017/sch_RI_03312017.rds", "06302017/sch_RI_06302017.rds")
+
     num <- length(reports)
 
     the_code <- 0
@@ -17,8 +18,7 @@ general_query <- function(ID, code){
     the_code[i] <- as.numeric(eval(parse(text = paste0("sch$", code))))
     rept_date[i] <- as.Date(strsplit(strsplit(reports[i], "_")[[1]][3], "\\.")[[1]][1], "%m%d%Y")
     }
-    ## result <- data.frame(as.numeric(the_code), row.names = date)
-    ## names(result) <- c("the_code")
+    
     result <- xts::as.xts(as.numeric(the_code), order.by = rept_date)
     names(result) <- quote(code)
     return(result)
