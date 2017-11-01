49774619c3307216bd81627c5e2030394e64535e
Matt Brigida <matt@complete-markets.com>
2017-10-31 20:11:14 -0400

    adding exog variables to mdi analysis

--- querying_data/analysis/mdi_sample_analysis.R
+++ querying_data/analysis/mdi_sample_analysis.R
@@ -80,4 +80,78 @@ write.csv(all_loans, "mdi_loans_cap.csv")
 saveRDS(all_loans, "mdi_loans_cap.rds")
 
 
-### Expgenous RHS variables ------
+### Exogenous RHS variables ------
+## pull data from FRED
+
+library(quantmod)
+library(tseries)
+
+## T10Y2YM -- 10 - 2 year treasury rate (monthly)
+## GS10 -- 10 year (monthly)
+## CPIAUCSL -- CPI season adj (monthly)
+## (PAYEMS) nonfarm payrolls season adj (monthly)
+
+ten_two <- getSymbols('T10Y2YM', src = "FRED", auto.assign = FALSE)
+ten_two <- to.quarterly(ten_two)
+ten_two <- (ten_two$ten_two.High + ten_two$ten_two.Low) / 2
+adf.test(ten_two) ## => stationary
+ten_two_unexp <- ar(ten_two)$resid
+
+ten <- getSymbols('GS10', src = "FRED", auto.assign = FALSE)
+ten <- to.quarterly(ten)
+ten <- (ten$ten.High + ten$ten.Low) / 2
+adf.test(ten) ## => nonstationary
+ten <- Delt(ten, type = 'log')[-1]
+adf.test(ten) ## => stationary
+ten_unexp <- ar(ten)$resid
+
+cpi <- getSymbols('CPIAUCSL', src = "FRED", auto.assign = FALSE)
+cpi <- to.quarterly(cpi)
+cpi <- (cpi$cpi.High + cpi$cpi.Low) / 2
+adf.test(cpi)                           # => nonstationary
+cpi <- Delt(cpi, type = 'log')[-1]
+adf.test(cpi)                           # => stationary
+cpi_unexp <- ar(cpi)$resid
+
+
+jobs <- getSymbols('PAYEMS', src = "FRED", auto.assign = FALSE)
+jobs <- to.quarterly(jobs)
+jobs <- (jobs$jobs.High + jobs$jobs.Low) / 2
+adf.test(jobs)                           # => nonstationary
+jobs <- Delt(jobs, type = 'log')[-1]
+adf.test(jobs)                           # => stationary
+jobs_unexp <- ar(jobs)$resid
+
+exog <- merge.xts(ten_two, ten_two_unexp, ten, ten_unexp, cpi, cpi_unexp, jobs, jobs_unexp)
+
+
+
+
+
+
+
+
+
+
+
+### Variables for analysis
+# Independent Variables
+# Exogenous
+# Interest Rates
+# Monthly GDP growth rate
+# Monthly Jobs report
+# CPI
+#  
+# Endogenous
+# Net Interest Margin
+# RWA
+# Tier 1 Capital
+# Total Capital
+# ROAA
+# Derivative Proxy
+#  
+# Dependent Variables
+# Loans/Assets
+# Res Mortg./Total Loans
+# PAR >90
+# NPLs
