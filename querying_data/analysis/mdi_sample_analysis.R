80f15e17799d0c9e3dcf7c36c9075e8aeca9d129
Matt Brigida <matt@complete-markets.com>
2017-11-04 23:24:25 -0400

    added commercial and ind loans to analysis

--- querying_data/analysis/mdi_sample_analysis.R
+++ querying_data/analysis/mdi_sample_analysis.R
@@ -81,6 +81,30 @@ write.csv(all_loans, "mdi_loans_cap.csv")
 saveRDS(all_loans, "mdi_loans_cap.rds")
 
 
+### Commercial and Indistrial Loans: RCON1766
+
+source("../individual_schedule_queries/schedule_RCCI_query.R")
+
+Cloans <- new.env()
+
+## firm type 1
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCON1766"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = Cloans)
+
+    }
+
+all_Cloans <- do.call(merge, as.list(Cloans))
+
+write.csv(all_Cloans, "mdi_Cloans_cap.csv")
+saveRDS(all_Cloans, "mdi_Cloans_cap.rds")
+
+
 ### Exogenous RHS variables ------
 ## pull data from FRED
 
