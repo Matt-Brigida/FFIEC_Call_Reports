727b9e2ce50a7a229e45216f24855552851e2c01
Matt Brigida <matt@complete-markets.com>
2017-12-28 13:52:15 -0500

    matched public firms, but missing many.

--- querying_data_and_analysis/analysis/matching_names_for_public_private/matching.R
+++ querying_data_and_analysis/analysis/matching_names_for_public_private/matching.R
@@ -1,9 +1,32 @@
 ### Matching names to get which banks are public -----
 
+## Banks in FDIC data
 por <- readRDS("../../../data/06302017/bulk_POR_06302017.rds")
 
-nameID <- data.frame(cbind(por$IDRSSD, por$'Financial Institution Name'), stringsAsFactors = FALSE)
+namesID <- data.frame(cbind(por$IDRSSD, por$'Financial Institution Name'), stringsAsFactors = FALSE)
 names(nameID) <- c("IDRSSD", "Name")
 
+## Listed Banks
 
-## remove INC, THE, FSB, F.S.B., 
+listed <- read.csv("./public_bank_names.txt")
+listed <- data.frame(listed[,1], stringsAsFactors = FALSE)
+listed_cleaned <- data.frame(gsub(pattern = " INC.", replacement = "", listed[,1]), stringsAsFactors = FALSE)
+listed_cleaned <- data.frame(gsub(pattern = " CORP.", replacement = "", listed_cleaned[,1]), stringsAsFactors = FALSE)
+listed_cleaned <- data.frame(gsub(pattern = " S.A.", replacement = "", listed_cleaned[,1]), stringsAsFactors = FALSE)
+listed_cleaned <- data.frame(gsub(pattern = " PLC.", replacement = "", listed_cleaned[,1]), stringsAsFactors = FALSE)
+listed_cleaned <- data.frame(gsub(pattern = " LTD.", replacement = "", listed_cleaned[,1]), stringsAsFactors = FALSE)
+
+## remove anything after the ,
+split <- strsplit(nameID[, 2], ",")
+namesID_cleaned <- unlist(lapply(split, `[[`, 1))
+namesID_cleaned <- data.frame(cbind(namesID[,1], namesID_cleaned), stringsAsFactors = FALSE)
+names(namesID_cleaned) <- c("IDRSSD", "Name")
+
+isin <- NA
+for (i in 1:length(namesID_cleaned[,2])){
+    isin[i] <- namesID_cleaned[i, 2] %in% listed_cleaned[,1]
+}
+
+ispublic <- data.frame(cbind(namesID_cleaned, isin), stringsAsFactors = FALSE)
+
+saveRDS(ispublic, "ispublic.rds")
