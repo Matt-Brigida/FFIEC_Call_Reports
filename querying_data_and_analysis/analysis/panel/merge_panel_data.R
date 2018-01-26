2488db7d815ebb52a19f4b2d6276ca75ebfc13e8
Matt Brigida <matt@complete-markets.com>
2018-01-26 18:00:03 -0500

    added comm and industrial loans to panel

--- querying_data_and_analysis/analysis/panel/merge_panel_data.R
+++ querying_data_and_analysis/analysis/panel/merge_panel_data.R
@@ -5,3 +5,7 @@ panel_total_assets <- readRDS("panel_total_assets.rds")
 
 panel <- merge(panel_total_assets, panel_total_equity, by = c("quarter", "IDRSSD"), all = FALSE)
 
+
+panel_Cloans_cap <- readRDS("panel_Cloans_cap.rds")
+
+panel <- merge(panel, panel_Cloans_cap, by = c("quarter", "IDRSSD"), all = FALSE)
--- querying_data_and_analysis/analysis/panel/reshaping_data_for_panel.R
+++ querying_data_and_analysis/analysis/panel/reshaping_data_for_panel.R
@@ -1,10 +1,5 @@
 
-data <- readRDS("total_equity.rds")
-
-### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
-
-panelify_env <- new.env()
-
+## function to reshape data for panel -----
 panelify <- function(x, value_name){
     for (i in 1:dim(x)[2]){
         assign("tmp", data.frame(cbind(1:length(x[, i]), gsub("ID_", "", names(x[, i])), as.vector(x[, i]))))
@@ -16,6 +11,18 @@ panelify <- function(x, value_name){
     }
 }
 
+
+
+
+## change for whatever data we are reshaping ------
+data <- readRDS("../Full_Sample/pulling_alive_2017/Cloans_cap.rds")
+
+### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
+
+panelify_env <- new.env()
+
+panelify(data, "Cloans_cap")
+
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_equity.rds")
+saveRDS(panelified, "panel_Cloans_cap.rds")
