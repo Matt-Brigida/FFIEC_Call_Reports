502a68100fa3d1d909ca1089bbae43087ba46d02
Matt Brigida <matt@complete-markets.com>
2018-02-03 12:02:46 -0500

    creating dead panel

---
+++ querying_data_and_analysis/analysis/Full_Sample/pulling_firms_dead_2001_2017/loan_data.R
@@ -0,0 +1,53 @@
+library(here)
+
+## sample of firms
+
+idrssds <- readRDS("./all_deaths.rds")
+
+### Total Loans and Leases: RCON2122
+
+source(paste0(here(), "/querying_data_and_analysis/individual_schedule_queries/schedule_RCCI_query.R"))
+
+loans<- new.env()
+
+## firm type 1
+for (i in idrssds){
+    
+    assign("tmpb1", RCCI_query(i, code = "RCON2122"))
+
+    names(tmpb1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpb1, envir = loans)
+
+    }
+
+all_loans <- do.call(merge, as.list(loans))
+
+write.csv(all_loans, "loans_cap.csv")
+saveRDS(all_loans, "loans_cap.rds")
+
+
+### Commercial and Indistrial Loans: RCON1766
+
+## source("../individual_schedule_queries/schedule_RCCI_query.R")
+
+Cloans <- new.env()
+
+## firm type 1
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCON1766"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = Cloans)
+
+    }
+
+all_Cloans <- do.call(merge, as.list(Cloans))
+
+write.csv(all_Cloans, "Cloans_cap.csv")
+saveRDS(all_Cloans, "Cloans_cap.rds")
+
--- querying_data_and_analysis/analysis/panel_dead/reshaping_data_for_panel.R
+++ querying_data_and_analysis/analysis/panel_dead/reshaping_data_for_panel.R
@@ -1,3 +1,4 @@
+library(xts)
 
 ## function to reshape data for panel -----
 panelify <- function(x, value_name){
@@ -18,7 +19,7 @@ panelify <- function(x, value_name){
 
 ## Cloans is all commercial and industrial loans----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/Cloans_cap.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/Cloans_cap.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -28,12 +29,12 @@ panelify(data, "Cloans_cap")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_Cloans_cap.rds")
+saveRDS(panelified, "panel_dead_Cloans_cap.rds")
 
 
 ## loans_cap is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/loans_cap.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/loans_cap.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -43,12 +44,12 @@ panelify(data, "loans_cap")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_loans_cap.rds")
+saveRDS(panelified, "panel_dead_loans_cap.rds")
 
 
 ## net_income is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/net_income.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/net_income.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -58,12 +59,12 @@ panelify(data, "net_income")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_net_income.rds")
+saveRDS(panelified, "panel_dead_net_income.rds")
 
 
 ## risk_weighted_assets is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/risk_weighted_assets.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/risk_weighted_assets.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -73,12 +74,12 @@ panelify(data, "risk_weighted_assets")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_risk_weighted_assets.rds")
+saveRDS(panelified, "panel_dead_risk_weighted_assets.rds")
 
 
 ## tier_1_cap is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/tier_1_cap.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/tier_1_cap.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -88,12 +89,12 @@ panelify(data, "tier_1_cap")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_tier_1_cap.rds")
+saveRDS(panelified, "panel_dead_tier_1_cap.rds")
 
 
 ## total_interest_expense is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/total_interest_expense.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/total_interest_expense.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -103,12 +104,12 @@ panelify(data, "total_interest_expense")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_interest_expense.rds")
+saveRDS(panelified, "panel_dead_total_interest_expense.rds")
 
 
 ## total_interest_income is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/total_interest_income.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/total_interest_income.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -118,12 +119,12 @@ panelify(data, "total_interest_income")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_interest_income.rds")
+saveRDS(panelified, "panel_dead_total_interest_income.rds")
 
 
 ## total_noninterest_expense is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/total_noninterest_expense.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/total_noninterest_expense.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -133,12 +134,12 @@ panelify(data, "total_noninterest_expense")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_noninterest_expense.rds")
+saveRDS(panelified, "panel_dead_total_noninterest_expense.rds")
 
 
 ## total_noninterest_income is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/total_noninterest_income.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/total_noninterest_income.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -148,14 +149,14 @@ panelify(data, "total_noninterest_income")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_noninterest_income.rds")
+saveRDS(panelified, "panel_dead_total_noninterest_income.rds")
 
 
 #### From income statement by loan type ------
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_loan_type/interest_income_1_4_residential_prop.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_loan_type/interest_income_1_4_residential_prop.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -165,12 +166,12 @@ panelify(data, "interest_income_1_4_residential_prop")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_interest_income_1_4_residential_prop.rds")
+saveRDS(panelified, "panel_dead_interest_income_1_4_residential_prop.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_loan_type/interest_income_all_other_real_estate.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_loan_type/interest_income_all_other_real_estate.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -180,12 +181,12 @@ panelify(data, "interest_income_all_other_real_estate")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_interest_income_all_other_real_estate.rds")
+saveRDS(panelified, "panel_dead_interest_income_all_other_real_estate.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_loan_type/interest_income_comm_and_ind_loans.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_loan_type/interest_income_comm_and_ind_loans.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -195,12 +196,12 @@ panelify(data, "interest_income_comm_and_ind_loans")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_interest_income_comm_and_ind_loans.rds")
+saveRDS(panelified, "panel_dead_interest_income_comm_and_ind_loans.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_loan_type/interest_income_credit_cards.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_loan_type/interest_income_credit_cards.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -210,12 +211,12 @@ panelify(data, "interest_income_credit_cards")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_interest_income_credit_cards.rds")
+saveRDS(panelified, "panel_dead_interest_income_credit_cards.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_loan_type/interest_income_other_personal.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_loan_type/interest_income_other_personal.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -225,13 +226,13 @@ panelify(data, "interest_income_other_personal")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_interest_income_other_personal.rds")
+saveRDS(panelified, "panel_dead_interest_income_other_personal.rds")
 
 #### From income_statement_by_noninterest_income
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_noninterest_income/deposit_account_charges.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_noninterest_income/deposit_account_charges.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -241,12 +242,12 @@ panelify(data, "noninterest_income_deposit_account_charges")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_noninterest_income_deposit_account_charges.rds")
+saveRDS(panelified, "panel_dead_noninterest_income_deposit_account_charges.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_noninterest_income/fees_annuity_sales.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_noninterest_income/fees_annuity_sales.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -256,12 +257,12 @@ panelify(data, "noninterest_income_fees_annuity_sales")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_noninterest_income_fees_annuity_sales.rds")
+saveRDS(panelified, "panel_dead_noninterest_income_fees_annuity_sales.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_noninterest_income/fiduciary_activities.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_noninterest_income/fiduciary_activities.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -271,12 +272,12 @@ panelify(data, "noninterest_income_fiduciary_activities")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_noninterest_income_fiduciary_activities.rds")
+saveRDS(panelified, "panel_dead_noninterest_income_fiduciary_activities.rds")
 
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_noninterest_income/inv_bank_advisory_underwriting.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_noninterest_income/inv_bank_advisory_underwriting.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -286,11 +287,11 @@ panelify(data, "noninterest_income_inv_bank_advisory_underwriting")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_noninterest_income_inv_bank_advisory_underwriting.rds")
+saveRDS(panelified, "panel_dead_noninterest_income_inv_bank_advisory_underwriting.rds")
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_noninterest_income/securities_brokerage_fees_comm.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_noninterest_income/securities_brokerage_fees_comm.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -300,11 +301,11 @@ panelify(data, "noninterest_income_securities_brokerage_fees_comm")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_noninterest_income_securities_brokerage_fees_comm.rds")
+saveRDS(panelified, "panel_dead_noninterest_income_securities_brokerage_fees_comm.rds")
 
 ##  is ... ----
 
-data <- readRDS("../Full_Sample/pulling_alive_2017/income_statement_by_noninterest_income/trading_revenue.rds")
+data <- readRDS("../Full_Sample/pulling_firms_dead_2001_2017/income_statement_by_noninterest_income/trading_revenue.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
@@ -314,4 +315,4 @@ panelify(data, "noninterest_income_trading_revenue")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_noninterest_income_trading_revenue.rds")
+saveRDS(panelified, "panel_dead_noninterest_income_trading_revenue.rds")
