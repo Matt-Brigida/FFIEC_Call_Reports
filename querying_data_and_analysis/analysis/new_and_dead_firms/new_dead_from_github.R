7c626c04089f1820e4f2e00db782fc84a3a57d20
Matt Brigida <mdbrigida@gmail.com>
2018-03-23 19:57:49 -0400

    new dead for example

---
+++ querying_data_and_analysis/analysis/new_and_dead_firms/new_dead_from_github.R
@@ -0,0 +1,50 @@
+quarters <- c("03312001", "06302001", "09302001", "12312001", "03312002", "06302002", "09302002", "12312002", "03312003", "06302003", "09302003", "12312003", "03312004", "06302004", "09302004", "12312004", "03312005", "06302005", "09302005", "12312005", "03312006", "06302006", "09302006", "12312006", "03312007", "06302007", "09302007", "12312007", "03312008", "06302008", "09302008", "12312008", "03312009", "06302009", "09302009", "12312009", "03312010", "06302010", "09302010", "12312010", "03312011", "06302011", "09302011", "12312011", "03312012", "06302012", "09302012", "12312012", "03312013", "06302013", "09302013", "12312013", "03312014", "06302014", "09302014", "12312014", "03312015", "06302015", "09302015", "12312015", "03312016", "06302016", "09302016", "12312016", "03312017", "06302017", "09302017", "12312017")
+
+idrssds <- list()
+
+for (q in quarters){
+tmp_bulk_por <- readRDS(url(paste0("https://github.com/Matt-Brigida/FFIEC_Call_Reports/blob/master/data/", q, "/bulk_POR_", q, ".rds?raw=true")))
+
+idrssds[[q]] <- tmp_bulk_por$IDRSSD
+    }
+
+### births
+births <- list()
+
+for (i in 2:length(quarters)){
+    births[[quarters[i]]] <- idrssds[[i]][!(idrssds[[i]] %in% idrssds[[i - 1]])]
+}
+
+### deaths
+deaths <- list()
+
+for (i in 1:(length(quarters) - 1)){
+    deaths[[quarters[i + 1]]] <- idrssds[[i]][!(idrssds[[i]] %in% idrssds[[i + 1]])]
+}
+
+## Vizualizations
+
+library(highcharter)
+
+### number births
+
+num_births <- unlist(lapply(births, length))
+
+### number deaths
+
+num_deaths <- unlist(lapply(deaths, length))
+
+### birth - death
+
+net_gain <- num_births - num_deaths
+
+### Plot
+
+plot_data <- data.frame(cbind(num_births, num_deaths, net_gain))
+names(plot_data) <- c("Births", "Deaths", "Net_Gain")
+
+hc <- hchart(plot_data, type = "column")    
+hc
+
+%>% hc_add_theme(hc_theme_flatdark())
+
