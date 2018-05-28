07cc359b5d9bd0f13be7312b0be5387ab48bb817
Matt Brigida <mdbrigida@gmail.com>
2018-05-28 15:25:51 -0400

    testing panel data setup

---
+++ querying_data_and_analysis/analyses/panel_all_firms/panel_alive_2017/test_panel_data_frame.R
@@ -0,0 +1,12 @@
+library(xts)
+library(plm)
+
+data <- readRDS("./panel.rds")
+
+dataP <- pdata.frame(data, index = c("IDRSSD", "quarter"), drop.index=TRUE, row.names=TRUE)
+
+testFEmodel <- plm(loans_cap ~  retained_earnings + tier_1_cap + total_noninterest_income, data = dataP, model = "within", effect = "twoways")
+
+summary(testFEmodel)
+
+### can SUCCESSFULLY run a FE model with plm!
