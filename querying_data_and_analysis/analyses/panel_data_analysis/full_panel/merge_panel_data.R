48e920afde58fe63766bf3369269dc443c7e1e27
Matt Brigida <mdbrigida@gmail.com>
2018-06-12 19:02:43 -0400

    testing FE models and cleaning

--- querying_data_and_analysis/analyses/panel_data_analysis/full_panel/merge_panel_data.R
+++ querying_data_and_analysis/analyses/panel_data_analysis/full_panel/merge_panel_data.R
@@ -3,39 +3,75 @@
 
 ## read in data and merge-----
 
-panel_total_equity <- readRDS("panel_total_equity.rds")
+panel_total_equity_lagged_1_year <- readRDS("panel_total_equity_lagged_1_year.rds")
 
-panel_total_assets <- readRDS("panel_total_assets.rds")
+panel_total_assets_lagged_1_year <- readRDS("panel_total_assets_lagged_1_year.rds")
 
-panel <- merge(panel_total_assets, panel_total_equity, by = c("quarter", "IDRSSD"), all = FALSE)
+panel <- merge(panel_total_assets_lagged_1_year, panel_total_equity_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
 
 ## t1 leverage ratio (none past 2014)------
 
-panel_t1_leverage_ratio_none_past_2014 <- readRDS("./panel_t1_leverage_ratio_none_past_2014.rds")
+panel_t1_LR_pre_2014_lagged_1_year <- readRDS("./panel_t1_LR_pre_2014_lagged_1_year.rds")
 
-panel <- merge(panel, panel_t1_leverage_ratio_none_past_2014, by = c("quarter", "IDRSSD"), all = FALSE)
+panel <- merge(panel, panel_t1_LR_pre_2014_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+## t1 RBCR pre 2014------
+
+panel_t1_RBCR_pre_2014_lagged_1_year <- readRDS("./panel_t1_RBCR_pre_2014_lagged_1_year.rds")
+
+panel <- merge(panel, panel_t1_RBCR_pre_2014_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+### Amount of SB loans lagged 1 year
+##
+
+panel_amt_CI_less_100_SB_loans_lagged_1_year <- readRDS("./panel_amt_CI_less_100_SB_loans_lagged_1_year.rds")
+
+panel <- merge(panel, panel_amt_CI_less_100_SB_loans_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
 
 ##
 
-panel_amt_CI_less_100_SB_loans.rds <- readRDS("./panel_amt_CI_less_100_SB_loans.rds")
+panel_amt_CI_100_250_SB_loans_lagged_1_year <- readRDS("./panel_amt_CI_100_250_SB_loans_lagged_1_year.rds")
 
-panel <- merge(panel, panel_amt_CI_less_100_SB_loans.rds, by = c("quarter", "IDRSSD"), all = FALSE)
-
-##
-
-panel_amt_CI_100_250_SB_loans <- readRDS("./panel_amt_CI_100_250_SB_loans.rds")
-
-panel <- merge(panel, panel_amt_CI_100_250_SB_loans, by = c("quarter", "IDRSSD"), all = FALSE)
+panel <- merge(panel, panel_amt_CI_100_250_SB_loans_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
 
 ##
 
-panel_amt_CI_250_1000_SB_loans <- readRDS("./panel_amt_CI_250_1000_SB_loans.rds")
+panel_amt_CI_250_1000_SB_loans_lagged_1_year <- readRDS("./panel_amt_CI_250_1000_SB_loans_lagged_1_year.rds")
 
-panel <- merge(panel, panel_amt_CI_250_1000_SB_loans, by = c("quarter", "IDRSSD"), all = FALSE)
+panel <- merge(panel, panel_amt_CI_250_1000_SB_loans_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
 
-## turn all columns to numeric from factor 
+### % change in SB loans 
+##
+
+panel_amt_CI_less_100_SB_loans_Delt <- readRDS("./panel_amt_CI_less_100_SB_loans_Delt.rds")
+
+panel <- merge(panel, panel_amt_CI_less_100_SB_loans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_amt_CI_100_250_SB_loans_Delt <- readRDS("./panel_amt_CI_100_250_SB_loans_Delt.rds")
+
+panel <- merge(panel, panel_amt_CI_100_250_SB_loans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_amt_CI_250_1000_SB_loans_Delt <- readRDS("./panel_amt_CI_250_1000_SB_loans_Delt.rds")
+
+panel <- merge(panel, panel_amt_CI_250_1000_SB_loans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+
+### turn all columns to numeric from factor 
 panel[] <- lapply(panel, function(x) as.numeric(as.character(x)))
-saveRDS(panel, "panel.rds")
 
 ### creating regression variables------
 
+panel$tot_SB_loans_lagged_1_year <- panel$amt_CI_less_100_SB_loans_lagged_1_year + panel$amt_CI_100_250_SB_loans_lagged_1_year + panel$amt_CI_250_1000_SB_loans_lagged_1_year
+
+panel$tot_lagged_SB_loans_TA <- panel$tot_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$less_100_lagged_SB_loans_TA <- panel$amt_CI_less_100_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$X100_250_lagged_SB_loans_TA <- panel$amt_CI_100_250_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$X250_1000_lagged_SB_loans_TA <- panel$amt_CI_250_1000_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+
+
+
+saveRDS(panel, "panel.rds")
--- querying_data_and_analysis/analyses/panel_data_analysis/full_panel/reshaping_data_for_panel.R
+++ querying_data_and_analysis/analyses/panel_data_analysis/full_panel/reshaping_data_for_panel.R
@@ -1,3 +1,4 @@
+library(xts)
 
 ## function to reshape data for panel -----
 panelify <- function(x, value_name){
@@ -12,94 +13,141 @@ panelify <- function(x, value_name){
 }
 
 
-library(xts)
-
 ## change for whatever data we are reshaping ------
 
 ## Total Assets----
 
-data <- readRDS("../get_panel_data/pulling_all_firms/data_BS/total_assets.rds")
+data <- readRDS("../get_panel_data/pulling_all_firms/data_BS/total_assets_lagged_1_year.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
 panelify_env <- new.env()
 
-panelify(data, "total_assets")
+panelify(data, "total_assets_lagged_1_year")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_assets.rds")
+saveRDS(panelified, "panel_total_assets_lagged_1_year.rds")
 
 
 ## Total Equity----
 
-data <- readRDS("../get_panel_data/pulling_all_firms/data_BS/total_equity.rds")
+data <- readRDS("../get_panel_data/pulling_all_firms/data_BS/total_equity_lagged_1_year.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
 panelify_env <- new.env()
 
-panelify(data, "total_equity")
+panelify(data, "total_equity_lagged_1_year")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_total_equity.rds")
+saveRDS(panelified, "panel_total_equity_lagged_1_year.rds")
 
 
 ## T1 leverage ratio----
 
-data <- readRDS("../get_panel_data/pulling_all_firms/data_capital_ratios/t1_leverage_ratio_none_past_2014.rds")
+data <- readRDS("../get_panel_data/pulling_all_firms/data_capital_ratios/t1_LR_pre_2014_lagged_1_year.rds")
 
 ### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
 
 panelify_env <- new.env()
 
-panelify(data, "t1_leverage_ratio_none_past_2014")
+panelify(data, "t1_LR_pre_2014_lagged_1_year")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_t1_leverage_ratio_none_past_2014.rds")
+saveRDS(panelified, "panel_t1_LR_pre_2014_lagged_1_year.rds")
+
+## T1 RBCR----
+
+data <- readRDS("../get_panel_data/pulling_all_firms/data_capital_ratios/t1_RBCR_pre_2014_lagged_1_year.rds")
+
+### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
+
+panelify_env <- new.env()
+
+panelify(data, "t1_RBCR_pre_2014_lagged_1_year")
+
+panelified <- do.call(rbind, as.list(panelify_env))
+
+saveRDS(panelified, "panel_t1_RBCR_pre_2014_lagged_1_year.rds")
+
+
+### % Change amount of SB loans
+## amt_CI_less_100_SB_loans----
+
+data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_less_100_SB_loans_Delt.rds")
+
+panelify_env <- new.env()
+
+panelify(data, "amt_CI_less_100_SB_loans_Delt")
+
+panelified <- do.call(rbind, as.list(panelify_env))
+
+saveRDS(panelified, "panel_amt_CI_less_100_SB_loans_Delt.rds")
 
 ## amt_CI_100_250_SB_loans----
 
-data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_100_250_SB_loans.rds")
-
-### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
+data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_100_250_SB_loans_Delt.rds")
 
 panelify_env <- new.env()
 
-panelify(data, "amt_CI_100_250_SB_loans")
+panelify(data, "amt_CI_100_250_SB_loans_Delt")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_amt_CI_100_250_SB_loans.rds")
+saveRDS(panelified, "panel_amt_CI_100_250_SB_loans_Delt.rds")
 
 ## amt_CI_250_1000_SB_loans----
 
-data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_250_1000_SB_loans.rds")
-
-### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
+data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_250_1000_SB_loans_Delt.rds")
 
 panelify_env <- new.env()
 
-panelify(data, "amt_CI_250_1000_SB_loans")
+panelify(data, "amt_CI_250_1000_SB_loans_Delt")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_amt_CI_250_1000_SB_loans.rds")
+saveRDS(panelified, "panel_amt_CI_250_1000_SB_loans_Delt.rds")
 
+
+### Amount of SB loans lagged 1 year
 ## amt_CI_less_100_SB_loans----
 
-data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_less_100_SB_loans.rds")
-
-### Reshaping data: x is the output from query (xts with IDRSSD in column name) ----
+data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_less_100_SB_loans_lagged_1_year.rds")
 
 panelify_env <- new.env()
 
-panelify(data, "amt_CI_less_100_SB_loans")
+panelify(data, "amt_CI_less_100_SB_loans_lagged_1_year")
 
 panelified <- do.call(rbind, as.list(panelify_env))
 
-saveRDS(panelified, "panel_amt_CI_less_100_SB_loans.rds")
+saveRDS(panelified, "panel_amt_CI_less_100_SB_loans_lagged_1_year.rds")
 
+## amt_CI_100_250_SB_loans----
+
+data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_100_250_SB_loans_lagged_1_year.rds")
+
+panelify_env <- new.env()
+
+panelify(data, "amt_CI_100_250_SB_loans_lagged_1_year")
+
+panelified <- do.call(rbind, as.list(panelify_env))
+
+saveRDS(panelified, "panel_amt_CI_100_250_SB_loans_lagged_1_year.rds")
+
+## amt_CI_250_1000_SB_loans----
+
+data <- readRDS("../get_panel_data/pulling_all_firms/data_small_business_loans/amt_CI_250_1000_SB_loans_lagged_1_year.rds")
+
+panelify_env <- new.env()
+
+panelify(data, "amt_CI_250_1000_SB_loans_lagged_1_year")
+
+panelified <- do.call(rbind, as.list(panelify_env))
+
+saveRDS(panelified, "panel_amt_CI_250_1000_SB_loans_lagged_1_year.rds")
+
+## T1 risk based capital ratio----
 
