b8aafc23fb631632a48032c990e15078e261a827
Matt Brigida <mdbrigida@gmail.com>
2018-08-19 20:03:30 -0400

    getting births for de novo ind

--- querying_data_and_analysis/analyses/panel_data_analysis/new_and_dead_firms/new_dead_firms.R
+++ querying_data_and_analysis/analyses/panel_data_analysis/new_and_dead_firms/new_dead_firms.R
@@ -31,6 +31,8 @@ for (i in 2:length(quarters)){
     births[[quarters[i]]] <- idrssds[[i]][!(idrssds[[i]] %in% idrssds[[i - 1]])]
 }
 
+saveRDS(as.vector(unlist(births)), "idrssd_births_through_12312017.rds")
+
 ### firms in quarter 3 that are not in quarter 4
 idrssds[[3]][!(idrssds[[3]] %in% idrssds[[4]])]
 
