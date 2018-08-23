635fccf72c044ba433f247f75ef8e26fe57e15d9
Matt Brigida <mdbrigida@gmail.com>
2018-08-23 16:57:50 -0400

    starting new data for roa analysis

---
+++ querying_data_and_analysis/analyses/panel_data_analysis/full_panel/1_panel_with_full_quarter_date/LHS_ROA_data_code/merge_panel_data.R
@@ -0,0 +1,266 @@
+## load libs
+
+library(xts)
+
+## read in panel when not yet loaded
+## panel <- readRDS('panel.rds')
+
+## read in data and merge-----
+
+panel_total_equity_lagged_1_year <- readRDS("panel_total_equity_lagged_1_year.rds")
+
+panel_total_assets_lagged_1_year <- readRDS("panel_total_assets_lagged_1_year.rds")
+
+panel <- merge(panel_total_assets_lagged_1_year, panel_total_equity_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+## t1 leverage ratio (none past 2014)------
+
+panel_t1_LR_lagged_1_year <- readRDS("./panel_t1_LR_lagged_1_year.rds")
+
+panel <- merge(panel, panel_t1_LR_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+## t1 RBCR pre 2014------
+
+panel_t1_RBCR_lagged_1_year <- readRDS("./panel_t1_RBCR_lagged_1_year.rds")
+
+panel <- merge(panel, panel_t1_RBCR_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+### Amount of SB loans lagged 1 year
+##
+
+panel_amt_CI_less_100_SB_loans_lagged_1_year <- readRDS("./panel_amt_CI_less_100_SB_loans_lagged_1_year.rds")
+
+panel <- merge(panel, panel_amt_CI_less_100_SB_loans_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_amt_CI_100_250_SB_loans_lagged_1_year <- readRDS("./panel_amt_CI_100_250_SB_loans_lagged_1_year.rds")
+
+panel <- merge(panel, panel_amt_CI_100_250_SB_loans_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_amt_CI_250_1000_SB_loans_lagged_1_year <- readRDS("./panel_amt_CI_250_1000_SB_loans_lagged_1_year.rds")
+
+panel <- merge(panel, panel_amt_CI_250_1000_SB_loans_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+### % change in SB loans 
+##
+
+panel_amt_CI_less_100_SB_loans_Delt <- readRDS("./panel_amt_CI_less_100_SB_loans_Delt.rds")
+
+panel <- merge(panel, panel_amt_CI_less_100_SB_loans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_amt_CI_100_250_SB_loans_Delt <- readRDS("./panel_amt_CI_100_250_SB_loans_Delt.rds")
+
+panel <- merge(panel, panel_amt_CI_100_250_SB_loans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_amt_CI_250_1000_SB_loans_Delt <- readRDS("./panel_amt_CI_250_1000_SB_loans_Delt.rds")
+
+panel <- merge(panel, panel_amt_CI_250_1000_SB_loans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_npa_30_89_lagged_1_year <- readRDS("./panel_npa_30_89_lagged_1_year.rds")
+
+panel <- merge(panel, panel_npa_30_89_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+
+##
+
+panel_npa_90_plus_lagged_1_year <- readRDS("./panel_npa_90_plus_lagged_1_year.rds")
+
+panel <- merge(panel, panel_npa_90_plus_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+
+##
+
+panel_npa_nonacc_lagged_1_year <- readRDS("./panel_npa_nonacc_lagged_1_year.rds")
+
+panel <- merge(panel, panel_npa_nonacc_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_net_income_lagged_1_year <- readRDS("./panel_net_income_lagged_1_year.rds")
+
+panel <- merge(panel, panel_net_income_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+panel_total_deposits_lagged_1_year <- readRDS("./panel_domestic_deposits_lagged_1_year.rds")
+
+panel <- merge(panel, panel_total_deposits_lagged_1_year, by = c("quarter", "IDRSSD"), all = FALSE)
+
+##
+
+totSBloans_Delt <- readRDS("./panel_totSBloans_Delt.rds")
+
+panel <- merge(panel, totSBloans_Delt, by = c("quarter", "IDRSSD"), all = FALSE)
+
+
+##########################################
+### Cleaning and arranging ###############
+##########################################
+
+### turn all columns to numeric from factor 
+panel[] <- lapply(panel, function(x) as.numeric(as.character(x)))
+
+## remove any Inf or NaNs in the change data
+panel$amt_CI_less_100_SB_loans_Delt[is.infinite(panel$amt_CI_less_100_SB_loans_Delt)] <- NA
+panel$amt_CI_less_100_SB_loans_Delt[is.nan(panel$amt_CI_less_100_SB_loans_Delt)] <- NA
+
+panel$amt_CI_100_250_SB_loans_Delt[is.infinite(panel$amt_CI_100_250_SB_loans_Delt)] <- NA
+panel$amt_CI_100_250_SB_loans_Delt[is.nan(panel$amt_CI_100_250_SB_loans_Delt)] <- NA
+
+panel$amt_CI_250_1000_SB_loans_Delt[is.infinite(panel$amt_CI_250_1000_SB_loans_Delt)] <- NA
+panel$amt_CI_250_1000_SB_loans_Delt[is.nan(panel$amt_CI_250_1000_SB_loans_Delt)] <- NA
+
+
+### creating regression variables------
+
+panel$tot_SB_loans_lagged_1_year <- panel$amt_CI_less_100_SB_loans_lagged_1_year + panel$amt_CI_100_250_SB_loans_lagged_1_year + panel$amt_CI_250_1000_SB_loans_lagged_1_year
+
+panel$tot_lagged_SB_loans_TA <- panel$tot_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$less_100_lagged_SB_loans_TA <- panel$amt_CI_less_100_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$X100_250_lagged_SB_loans_TA <- panel$amt_CI_100_250_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$X250_1000_lagged_SB_loans_TA <- panel$amt_CI_250_1000_SB_loans_lagged_1_year / panel$total_assets_lagged_1_year
+panel$ROA <- panel$net_income_lagged_1_year / panel$total_assets_lagged_1_year
+panel$tot_NPA <- panel$npa_30_89_lagged_1_year + panel$npa_90_plus_lagged_1_year + panel$npa_nonacc_lagged_1_year
+panel$NPA_TA <- panel$tot_NPA / panel$total_assets_lagged_1_year
+panel$TD_TA <- panel$domestic_deposits_lagged_1_year / panel$total_assets_lagged_1_year
+
+### create time indicator variables
+
+## post-crisis indicator for 2012 to 2015
+
+panel$post_crisis_ind <- ifelse(panel$quarter %in% 45:60, 1, 0)
+
+theindex_panel <- paste0(panel$quarter, "_", panel$IDRSSD)
+panel <- cbind(theindex_panel, panel)
+
+saveRDS(panel, "panel.rds")
+
+### creating MDI insicators and subsets-------------
+
+### preparing MDI data -----
+
+mdi <- read.csv("../new_jim_mdi_dataset.csv", header = TRUE, stringsAsFactors = FALSE, na.strings=c(""," ","NA"))
+mdi$CITY <- na.locf(mdi$CITY)
+mdi$STATE <- na.locf(mdi$STATE)
+mdi$MinorityStatusCode <- na.locf(mdi$MinorityStatusCode)
+mdi$DATE <- as.Date(mdi$DATE, format = "%m/%d/%Y")
+mdi$DATE <- as.numeric(gsub("-", "", mdi$DATE))
+
+## remove all mdi data after 20140630  which is the last observation in Rebel dataset -----
+## mdi <- mdi[mdi$DATE <= 20140630, ]
+
+## now create the matching key-----date_idrssd
+
+theindex <- paste0(mdi$DATE, "_", mdi$IDRSSD)
+
+mdi <- cbind(theindex, mdi)
+
+## split into race subsets-----
+
+mdi_black <- subset(mdi, MinorityStatusCode == "B")
+mdi_hispanic <- subset(mdi, MinorityStatusCode == "H")
+mdi_asian <- subset(mdi, MinorityStatusCode == "A")
+mdi_native <- subset(mdi, MinorityStatusCode == "N")
+mdi_multi <- subset(mdi, MinorityStatusCode == "M")
+
+### Create subsets--------
+
+panel_non_mdi <- subset(panel, !(theindex_panel %in% mdi$theindex))
+panel_mdi <- subset(panel, (theindex_panel %in% mdi$theindex))
+panel_mdi_black <- subset(panel, (theindex_panel %in% mdi_black$theindex))
+panel_mdi_hispanic <- subset(panel, (theindex_panel %in% mdi_hispanic$theindex))
+panel_mdi_asian <- subset(panel, (theindex_panel %in% mdi_asian$theindex))
+panel_mdi_native <- subset(panel, (theindex_panel %in% mdi_native$theindex))
+panel_mdi_multi <- subset(panel, (theindex_panel %in% mdi_multi$theindex))
+panel_mdi_bhn <- rbind(panel_mdi_black, panel_mdi_hispanic, panel_mdi_native)
+
+
+### create MDI indicator on full sample-----
+
+mdi_ind <- 0
+bhn_ind <- 0
+hispanic_ind <- 0
+asian_ind <- 0
+black_ind <- 0
+native_ind <- 0
+multi_ind <- 0
+
+## all mdi
+for (i in 1:dim(panel)[1]){
+    mdi_ind[i] <- ifelse(panel$theindex_panel[i] %in% panel_mdi$theindex_panel, 1, 0)
+}
+
+panel_mdi_ind <- cbind(panel, mdi_ind)
+
+## asian mdi
+for (i in 1:dim(panel)[1]){
+    asian_ind[i] <- ifelse(panel$theindex_panel[i] %in% panel_mdi_asian$theindex_panel, 1, 0)
+}
+
+panel_mdi_ind <- cbind(panel_mdi_ind, asian_ind)
+
+## non asian mdi
+for (i in 1:dim(panel)[1]){
+    bhn_ind[i] <- ifelse(panel$theindex_panel[i] %in% panel_mdi_bhn$theindex_panel, 1, 0)
+}
+
+panel_mdi_ind <- cbind(panel_mdi_ind, bhn_ind)
+
+## black mdi
+for (i in 1:dim(panel)[1]){
+    black_ind[i] <- ifelse(panel$theindex_panel[i] %in% panel_mdi_black$theindex_panel, 1, 0)
+}
+
+panel_mdi_ind <- cbind(panel_mdi_ind, black_ind)
+
+## hispanic mdi
+for (i in 1:dim(panel)[1]){
+    hispanic_ind[i] <- ifelse(panel$theindex_panel[i] %in% panel_mdi_hispanic$theindex_panel, 1, 0)
+}
+
+panel_mdi_ind <- cbind(panel_mdi_ind, hispanic_ind)
+
+
+
+### add mdi type by name to all data------------------------------------------------
+
+mdi_type <- ""
+
+for (i in 1:dim(panel)[1]){
+    mdi_type[i] <- if(panel$theindex_panel[i] %in% panel_mdi_asian$theindex_panel){
+                       "MDI_Asian"
+                   } else {
+                       if(panel$theindex_panel[i] %in% panel_mdi_black$theindex_panel){
+                           "MDI_African_American"
+                       } else {
+                           if(panel$theindex_panel[i] %in% panel_mdi_hispanic$theindex_panel){
+                               "MDI_Hispanic"
+                           } else {
+                               if(panel$theindex_panel[i] %in% panel_mdi_native$theindex_panel){
+                                   "MDI_Native_American"
+                               } else {
+                                   if(panel$theindex_panel[i] %in% panel_mdi_multi$theindex_panel){
+                                       "MDI_Multi"
+                                   } else {
+                                       "Non-MDI"
+                                   }
+                               }
+                           }
+                       }
+                   }
+}
+
+
+panel_mdi_type <- cbind(panel, mdi_type)
+saveRDS(panel_mdi_type, "panel_mdi_type.rds")
+saveRDS(panel_mdi_ind, "panel_mdi_ind.rds")
+
---
+++ querying_data_and_analysis/analyses/panel_data_analysis/full_panel/1_panel_with_full_quarter_date/LHS_ROA_data_code/summary_stats.R
@@ -0,0 +1,164 @@
+## library(plm)
+library(tidyverse)
+
+data <- readRDS("./panel_mdi_type.rds")
+
+names(data)
+
+unique(data$mdi_type)
+
+### Split data-----
+
+data_non_mdi <- data[data$mdi_type == "Non-MDI", ][, -length(names(data))]
+data_aa <- data[data$mdi_type == "MDI_African_American", ][, -length(names(data))]
+data_his <- data[data$mdi_type == "MDI_Hispanic", ][, -length(names(data))]
+data_asian <- data[data$mdi_type == "MDI_Asian", ][, -length(names(data))]
+data_na <- data[data$mdi_type == "MDI_Native_American", ][, -length(names(data))]
+data_multi <- data[data$mdi_type == "MDI_Multi", ][, -length(names(data))]
+
+### create complete cases data
+data_non_mdi_cc <- data_non_mdi[complete.cases(data_non_mdi), ]
+data_aa_cc <- data_aa[complete.cases(data_aa), ]
+data_his_cc <- data_his[complete.cases(data_his), ]
+data_asian_cc <- data_asian[complete.cases(data_asian), ]
+data_na_cc <- data_na[complete.cases(data_na), ]
+data_multi_cc <- data_multi[complete.cases(data_multi), ]
+
+### remove unnecessary columns-------
+var <- c("theindex_panel", "quarter", "IDRSSD", "npa_30_89_lagged_1_year", "npa_90_plus_lagged_1_year", "npa_nonacc_lagged_1_year", "net_income_lagged_1_year", "domestic_deposits_lagged_1_year", "tot_SB_loans_lagged_1_year", "tot_NPA", "post_crisis_ind", "mdi_ind", "asian_ind", "bhn_ind", "black_ind", "hispanic_ind")
+
+data_non_mdi_cc <- data.frame(data_non_mdi_cc[, !(names(data_non_mdi_cc) %in% var)])
+data_aa_cc <- data.frame(data_aa_cc[, !(names(data_aa_cc) %in% var)])
+data_his_cc <- data.frame(data_his_cc[, !(names(data_his_cc) %in% var)])
+
+
+### least amount of variables----more observations when we run models because we ony do so for one SB loan size at aa time.
+dim(data_non_mdi_cc)[1] + dim(data_aa_cc)[1] + dim(data_his_cc)[1] + dim(data_asian_cc)[1] + dim(data_na_cc)[1] + dim(data_multi_cc)[1]
+## [1] 179956
+
+### Calculate medians
+data_aa_cc <- data.frame(apply(data_aa_cc, 2, as.numeric))
+medians_aa <- data.frame(cbind(apply(data_aa_cc, 2, median), "African_American"))
+names(medians_aa) <- c("value", "tag")
+
+data_his_cc <- data.frame(apply(data_his_cc, 2, as.numeric))
+medians_his <- data.frame(cbind(apply(data_his_cc, 2, median), "Hispanic"))
+names(medians_his) <- c("value", "tag")
+
+data_non_mdi_cc <- data.frame(apply(data_non_mdi_cc, 2, as.numeric))
+medians_non_mdi <- data.frame(cbind(apply(data_non_mdi_cc, 2, median), "Non-MDI"))
+names(medians_non_mdi) <- c("value", "tag")
+
+medians <- cbind(as.numeric(as.character(medians_aa$value)), as.numeric(as.character(medians_his$value)), as.numeric(as.character(medians_non_mdi$value)))
+medians <- as.tibble(medians)
+medians <- cbind(medians, as.character(rownames(medians_non_mdi)))
+names(medians) <- c("African_American", "Hispanic", "Non_MDI", "Measure")
+medians <- as.tibble(medians)
+
+medians_ta_te <- medians[c(1,2), ]
+medians <- medians[-c(1,2), ]
+medians_amt_loans <- medians[c(3, 4, 5), ]
+medians <- medians[-c(3, 4, 5), ]
+medians_TD <- medians[12, ]
+medians <- medians[-12, ]
+
+## name and round
+medians$Measure <- c("T1_Leverage_Ratio", "T1_Risk_Based_Capital_Ratio", "Change_Small_SB", "Change_Medium_SB", "Change_Large_SB", "Total_SB_over_TA", "Small_SB_over_TA", "Medium_SB_over_TA", "Large_SB_over_TA", "ROA", "Non_Performing_Assets_over_TA")
+medians$African_American <- round(medians$African_American, 4)
+medians$Hispanic <- round(medians$Hispanic, 4)
+medians$Non_MDI <- round(medians$Non_MDI, 4)
+
+medians_ta_te$Measure <- c("Total_Assets", "Total_Equity")
+medians_ta_te$African_American <- round(medians_ta_te$African_American, 4)
+medians_ta_te$Hispanic <- round(medians_ta_te$Hispanic, 4)
+medians_ta_te$Non_MDI <- round(medians_ta_te$Non_MDI, 4)
+
+medians_amt_loans$Measure <- c("Amount_Small_SB_Loans", "Amount_Medium_SB_Loans", "Amount_Large_SB_Loans")
+medians_amt_loans$African_American <- round(medians_amt_loans$African_American, 4)
+medians_amt_loans$Hispanic <- round(medians_amt_loans$Hispanic, 4)
+medians_amt_loans$Non_MDI <- round(medians_amt_loans$Non_MDI, 4)
+
+medians_TD$Measure <- c("Deposits_over_TA")
+medians_TD$African_American <- round(medians_TD$African_American, 4)
+medians_TD$Hispanic <- round(medians_TD$Hispanic, 4)
+medians_TD$Non_MDI <- round(medians_TD$Non_MDI, 4)
+
+## create a bar chart like the one here:  http://jkunst.com/highcharter/highcharts-api.html
+library(highcharter)
+
+## first set
+hc <- highchart() %>% 
+  hc_xAxis(categories = medians$Measure) %>% 
+  hc_add_series(name = "African_American", data = medians$African_American) %>% 
+  hc_add_series(name = "Hispanic", data = medians$Hispanic) %>% 
+  hc_add_series(name = "Non_MDI", data = medians$Non_MDI)
+
+hc <- hc %>% 
+  hc_chart(type = "column",
+           options3d = list(enabled = TRUE, beta = 15, alpha = 15))
+
+hc <- hc %>%
+    hc_title(text = "Median Values",
+                style = list(fontWeight = "bold"))
+
+hc
+
+library(htmlwidgets)
+saveWidget(hc, file="first_medians.html")
+
+## ta te
+hc <- highchart() %>% 
+  hc_xAxis(categories = medians_ta_te$Measure) %>% 
+  hc_add_series(name = "African_American", data = medians_ta_te$African_American) %>% 
+  hc_add_series(name = "Hispanic", data = medians_ta_te$Hispanic) %>% 
+  hc_add_series(name = "Non_MDI", data = medians_ta_te$Non_MDI)
+
+hc <- hc %>% 
+  hc_chart(type = "column",
+           options3d = list(enabled = TRUE, beta = 15, alpha = 15))
+
+hc <- hc %>%
+    hc_title(text = "Median Values",
+                style = list(fontWeight = "bold"))
+
+hc
+
+saveWidget(hc, file="medians_ta_te.html")
+
+## amt loans
+hc <- highchart() %>% 
+  hc_xAxis(categories = medians_amt_loans$Measure) %>% 
+  hc_add_series(name = "African_American", data = medians_amt_loans$African_American) %>% 
+  hc_add_series(name = "Hispanic", data = medians_amt_loans$Hispanic) %>% 
+  hc_add_series(name = "Non_MDI", data = medians_amt_loans$Non_MDI)
+
+hc <- hc %>% 
+  hc_chart(type = "column",
+           options3d = list(enabled = TRUE, beta = 15, alpha = 15))
+
+hc <- hc %>%
+    hc_title(text = "Median Values",
+                style = list(fontWeight = "bold"))
+
+hc
+
+saveWidget(hc, file="medians_amt_loans.html")
+
+## Total Deposits
+hc <- highchart() %>% 
+  hc_xAxis(categories = medians_TD$Measure) %>% 
+  hc_add_series(name = "African_American", data = medians_TD$African_American) %>% 
+  hc_add_series(name = "Hispanic", data = medians_TD$Hispanic) %>% 
+  hc_add_series(name = "Non_MDI", data = medians_TD$Non_MDI)
+
+hc <- hc %>% 
+  hc_chart(type = "column",
+           options3d = list(enabled = TRUE, beta = 15, alpha = 15))
+
+hc <- hc %>%
+    hc_title(text = "Median Values",
+                style = list(fontWeight = "bold"))
+
+hc
+
+saveWidget(hc, file="medians_TD.html")
