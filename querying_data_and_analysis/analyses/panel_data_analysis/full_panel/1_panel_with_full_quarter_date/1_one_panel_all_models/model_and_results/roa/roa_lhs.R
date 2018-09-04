bca8ae6cdd19d0586394a4837115157cbe7cfd59
Matt Brigida <mdbrigida@gmail.com>
2018-09-04 18:12:15 -0400

    updated ROA

--- querying_data_and_analysis/analyses/panel_data_analysis/full_panel/1_panel_with_full_quarter_date/1_one_panel_all_models/model_and_results/roa/roa_lhs.R
+++ querying_data_and_analysis/analyses/panel_data_analysis/full_panel/1_panel_with_full_quarter_date/1_one_panel_all_models/model_and_results/roa/roa_lhs.R
@@ -2,7 +2,7 @@
 library(plm)
 library(stargazer)
 
-panel <- readRDS("../full_panel.rds")
+panel <- readRDS("../../full_panel.rds")
 
 panel <- panel[, c("quarter", "totSBloans_Delt_lagged_1_year", "t1_LR_lagged_1_year", "tot_SB_loans_TA_lagged_1", "ROA", "NPA_TA_lagged_1", "total_assets_lagged_1_year", "TD_TA_lagged_1", "african_am_ind", "hispanic_ind", "de_novo", "TETA_lagged_1_year", "post_crisis_ind", "fin_crisis_ind")]
 
@@ -21,4 +21,4 @@ FEmodel3b <- plm(ROA ~  t1_LR_lagged_1_year + I(t1_LR_lagged_1_year * african_am
 summary3b <- summary(FEmodel3b)
 
 
-stargazer(FEmodel1a, FEmodel2b, FEmodel3b, covariate.labels = c("T1LR", "T1LR * AA", "T1LR * His", "% Change in Small Business Loans", "Small Business Loans", "NPA", "ln(TA)", "Deposits", "Post Crisis", "Fin Crisis", "De Novo", "ln(TA) * NPA", "TE"), dep.var.labels = "ROA", digits = 3, no.space=TRUE, header=FALSE, type='html', omit.stat=c("LL"), title = "All Banks: Determinants of ROA", out = "tables123.htm", intercept.bottom = TRUE, notes = "Results are from fixed-effects models with bank fixed effects, for the years 2001 through 2017.  Data are quarterly.  The dependent variable is Return on Assets. Business loans are defined as the sum of commercial, industrial, and commercial real-estate loans.  All variables are lagged one year realtive to ROA.")
+stargazer(FEmodel1a, FEmodel2b, FEmodel3b, covariate.labels = c("T1LR", "T1LR * AA", "T1LR * His", "% Change in Small Business Loans", "Small Business Loans", "NPA", "ln(TA)", "Deposits", "Post Crisis", "Fin Crisis", "De Novo", "ln(TA) * NPA", "TE"), dep.var.labels = "ROA", digits = 3, no.space=TRUE, header=FALSE, type='html', omit.stat=c("LL"), title = "All Banks: Determinants of ROA", out = "tables123.htm", intercept.bottom = TRUE, notes = "Results are from fixed-effects models with bank fixed effects, for the years 2001 through 2017.  Data are quarterly.  The dependent variable is Return on Assets. Business loans are defined as the sum of commercial, industrial, and commercial real-estate loans.  All variables are lagged one year relative to ROA.")
