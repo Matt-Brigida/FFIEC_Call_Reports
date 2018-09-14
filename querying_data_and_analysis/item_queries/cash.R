db0618823a1de9883e22852cc9200717461f034a
Matt Brigida <mdbrigida@gmail.com>
2018-09-14 17:12:38 -0400

    starting cash query script

---
+++ querying_data_and_analysis/item_queries/cash.R
@@ -0,0 +1,6 @@
+## Getting cash balance from balance sheet ----
+
+## Cash are in codes RCON0071 and RCON0081
+## ^^ check to be sure for some banks it is not under RCFD0071 and RCFD0081.  If so we have to query all.  Will just query all to be sure :)
+
+## All codes are from schedule RC, the bakance sheet.
