c47b3dacf7e657b280c910b35d4ef10c306c5f0c
Matt Brigida <mdbrigida@gmail.com>
2018-09-28 20:03:19 -0400

    saved as pdf

--- querying_data_and_analysis/analyses/panel_data_analysis/full_panel/1_panel_with_full_quarter_date/1_one_panel_all_models/model_and_results/sb/sb_lhs_data_investigation.R
+++ querying_data_and_analysis/analyses/panel_data_analysis/full_panel/1_panel_with_full_quarter_date/1_one_panel_all_models/model_and_results/sb/sb_lhs_data_investigation.R
@@ -14,19 +14,7 @@ panel <- panel[complete.cases(panel), ]
 
 
 ## viz data times ------
-barplot(prop.table(table(panel$quarter)))
+pdf("sample_frequency_hitogram.pdf")
+barplot(prop.table(table(panel$quarter)), xlab = "Quarter", ylab = "% of Sample", main = "Frequency of Our Sample by Quarter")
+dev.off()
 
-
-FEmodel1 <- plm(totSBloans_Delt ~  t1_LR_lagged_1_year + + I(t1_LR_lagged_1_year * african_am_ind) + I(t1_LR_lagged_1_year * hispanic_ind) + TETA_lagged_1_year + tot_SB_loans_TA_lagged_1 + ROA_lagged_1 + NPA_TA_lagged_1 + I(log(panel$total_assets_lagged_1_year)) + TD_TA_lagged_1 + post_crisis_ind + fin_crisis_ind + de_novo, data = panel, model = "within", effect = "individual")
-
-## summary(FEmodel1)
-
-FEmodel2 <- plm(totSBloans_Delt ~  t1_LR_lagged_1_year + I(t1_LR_lagged_1_year * african_am_ind) + I(t1_LR_lagged_1_year * hispanic_ind) + TETA_lagged_1_year + tot_SB_loans_TA_lagged_1 + ROA_lagged_1 + NPA_TA_lagged_1 + I(log(panel$total_assets_lagged_1_year))  + TD_TA_lagged_1 + post_crisis_ind + fin_crisis_ind + de_novo + I(log(panel$total_assets_lagged_1_year) * ROA_lagged_1), data = panel, model = "within", effect = "individual")
-
-## summary(FEmodel2)
-
-FEmodel3 <- plm(totSBloans_Delt ~  t1_LR_lagged_1_year + I(t1_LR_lagged_1_year * african_am_ind) + I(t1_LR_lagged_1_year * hispanic_ind) + TETA_lagged_1_year + tot_SB_loans_TA_lagged_1 + ROA_lagged_1 + NPA_TA_lagged_1 + I(log(panel$total_assets_lagged_1_year)) + TD_TA_lagged_1 + post_crisis_ind + fin_crisis_ind + de_novo + I(log(panel$total_assets_lagged_1_year) * ROA_lagged_1) + I(log(panel$total_assets_lagged_1_year) * NPA_TA_lagged_1), data = panel, model = "within", effect = "individual")
-
-## summary(FEmodel3)
-
-stargazer(FEmodel1, FEmodel2, FEmodel3, covariate.labels = c("T1LR", "T1LR * AA", "T1LR * His", "TE", "Small Business Loans", "ROA", "NPA", "ln(TA)", "Deposits", "Post Crisis", "Fin Crisis", "De Novo", "ln(TA) * ROA", "ln(TA) * NPA"), dep.var.labels = "% Change in Amt. SB Loans", digits = 3, no.space=TRUE, header=FALSE, type='html', omit.stat=c("LL"), title = "All Banks: Determinants of the % Change in the Amount of Small-Business Loans", out = "tablesChangeASBL.htm", intercept.bottom = TRUE, notes = "Results are from fixed-effects models with bank fixed effects, for the years 2001 through 2017.  Data are quarterly.  The dependent variable is percent change in the amount of small-business loans.  Small-Business loans are defined as the sum of commercial, industrial, and commercial real-estate loans.  All variables are lagged one year relative to the dependent variable.")
