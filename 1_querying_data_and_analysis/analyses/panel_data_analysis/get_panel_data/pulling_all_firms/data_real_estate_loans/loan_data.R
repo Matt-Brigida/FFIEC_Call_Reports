44ac481ada2ce93281638f36951964ca4abd5ecb
Matt Brigida <mdbrigida@gmail.com>
2018-10-24 21:07:54 -0400

    queries for rest of real estate loan data

--- 1_querying_data_and_analysis/analyses/panel_data_analysis/get_panel_data/pulling_all_firms/data_real_estate_loans/loan_data.R
+++ 1_querying_data_and_analysis/analyses/panel_data_analysis/get_panel_data/pulling_all_firms/data_real_estate_loans/loan_data.R
@@ -97,3 +97,106 @@ all_revolving <- do.call(merge, as.list(revolving))
 
 write.csv(all_revolving, "revolving.csv")
 saveRDS(all_revolving, "revolving.rds")
+
+
+### closed end first: RCON5367
+
+closed_end_first <- new.env()
+
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCON5367"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = closed_end_first)
+
+    }
+
+all_closed_end_first <- do.call(merge, as.list(closed_end_first))
+
+write.csv(all_closed_end_first, "closed_end_first.csv")
+saveRDS(all_closed_end_first, "closed_end_first.rds")
+
+
+### closed end junior: RCON5368
+
+closed_end_junior <- new.env()
+
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCON5368"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = closed_end_junior)
+
+    }
+
+all_closed_end_junior <- do.call(merge, as.list(closed_end_junior))
+
+write.csv(all_closed_end_junior, "closed_end_junior.csv")
+saveRDS(all_closed_end_junior, "closed_end_junior.rds")
+
+### multifamily: RCON1460
+
+multifamily <- new.env()
+
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCON1460"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = multifamily)
+
+    }
+
+all_multifamily <- do.call(merge, as.list(multifamily))
+
+write.csv(all_multifamily, "multifamily.csv")
+saveRDS(all_multifamily, "multifamily.rds")
+
+### owner_occupied_nonf_nonr: RCONF160
+
+owner_occupied_nonf_nonr <- new.env()
+
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCONF160"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = owner_occupied_nonf_nonr)
+
+    }
+
+all_owner_occupied_nonf_nonr <- do.call(merge, as.list(owner_occupied_nonf_nonr))
+
+write.csv(all_owner_occupied_nonf_nonr, "owner_occupied_nonf_nonr.csv")
+saveRDS(all_owner_occupied_nonf_nonr, "owner_occupied_nonf_nonr.rds")
+
+
+### other_nonf_nonr: RCONF161
+
+other_nonf_nonr <- new.env()
+
+for (i in idrssds){
+    
+    assign("tmpc1", RCCI_query(i, code = "RCONF161"))
+
+    names(tmpc1) <- paste0("ID_", i)
+
+    assign(paste0("ID_", i),
+           tmpc1, envir = other_nonf_nonr)
+
+    }
+
+all_other_nonf_nonr <- do.call(merge, as.list(other_nonf_nonr))
+
+write.csv(all_other_nonf_nonr, "other_nonf_nonr.csv")
+saveRDS(all_other_nonf_nonr, "other_nonf_nonr.rds")
