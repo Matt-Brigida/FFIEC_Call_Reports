e1e9af20e55ecef5533d005bda45be74a9538543
Matt Brigida <mdbrigida@gmail.com>
2018-11-06 18:41:04 -0500

    ML python setup

--- 3_machine_learning/python/tf_keras/wide_deep_model_keras.py
+++ 3_machine_learning/python/tf_keras/wide_deep_model_keras.py
@@ -8,6 +8,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import scipy as sc
+from scipy import stats
 
 ## translate the following to python-------
 
@@ -17,37 +18,26 @@ import scipy as sc
 
 
 readRDS = robjects.r['readRDS']
-df = readRDS('full_panel.rds')
+df = readRDS('./3_machine_learning/python/tf_keras/full_panel.rds')
 df = pandas2ri.ri2py(df)
 
 
-df.head()
-df1 = df[["quarter", "totSBloans_Delt", "t1_LR_lagged_1_year", "tot_SB_loans_TA_lagged_1", "ROA_lagged_1", "NPA_TA_lagged_1", "total_assets_lagged_1_year", "TD_TA_lagged_1", "african_am_ind", "hispanic_ind", "de_novo", "TETA_lagged_1_year", "post_crisis_ind", "fin_crisis_ind"]]
-
-df1.head()
+df1 = df[["IDRSSD", "quarter", "totSBloans_Delt", "t1_LR_lagged_1_year", "tot_SB_loans_TA_lagged_1", "ROA_lagged_1", "NPA_TA_lagged_1", "total_assets_lagged_1_year", "TD_TA_lagged_1", "african_am_ind", "hispanic_ind", "de_novo", "TETA_lagged_1_year", "post_crisis_ind", "fin_crisis_ind"]]
 
 df2 = df1.dropna(how='any')
 
-df2.head()
-
-
-
-
 ## create interaction and transformed variables --------
 
-df2['SB_AA_Int'] = df2['tot_SB_loans_TA_lagged_1'] * df2['african_am_ind']
+df2['T1_AA_Int'] = df2['t1_LR_lagged_1_year'] * df2['african_am_ind']
+df2['T1_H_Int'] = df2['t1_LR_lagged_1_year'] * df2['hispanic_ind']
 
-df2['SB_H_Int'] = df2['tot_SB_loans_TA_lagged_1'] * df2['hispanic_ind']
+# df2['SB_H_Int'] = df2['tot_SB_loans_TA_lagged_1'] * df2['hispanic_ind']
 df2['LN_TA'] = np.log(df2['total_assets_lagged_1_year'])
-
-sum(df2['SB_AA_Int'])
-sum(df2['SB_H_Int'])
-sum(df2['total_assets_lagged_1_year'])
-sum(df2['LN_TA'])
 
 ## now orthogonalize TE wrt t1 leverage ratio----
 
-df2.head()
+stats.linregress(df2["t1_LR_lagged_1_year"], df2["TETA_lagged_1_year"])
 
+df2.head()
 
 ## divide into training and testing data total_assets_lagged_1_year
