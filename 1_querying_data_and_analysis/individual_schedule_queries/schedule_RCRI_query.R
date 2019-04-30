3548e64d8f1b3344911073155d7c238391bd966a
Matt Brigida <mdbrigida@gmail.com>
2019-04-30 14:50:35 -0400

    updated schedule queries to 2018

--- 1_querying_data_and_analysis/individual_schedule_queries/schedule_RCRI_query.R
+++ 1_querying_data_and_analysis/individual_schedule_queries/schedule_RCRI_query.R
@@ -6,7 +6,7 @@ RCRI_query <- function(ID, code){
   ## this function can query any variable on the schedule RCR_1_of_2 -----
   ## arguments to the function are ID (the firm's IDRSSD) and the variable code (starting wtih RD...)
 
-    reports <- c("03312015/sch_RCRI_03312015.rds", "06302015/sch_RCRI_06302015.rds", "09302015/sch_RCRI_09302015.rds", "12312015/sch_RCRI_12312015.rds", "03312016/sch_RCRI_03312016.rds", "06302016/sch_RCRI_06302016.rds", "09302016/sch_RCRI_09302016.rds", "12312016/sch_RCRI_12312016.rds", "03312017/sch_RCRI_03312017.rds", "06302017/sch_RCRI_06302017.rds",  "09302017/sch_RCRI_09302017.rds", "12312017/sch_RCRI_12312017.rds")
+    reports <- c("03312015/sch_RCRI_03312015.rds", "06302015/sch_RCRI_06302015.rds", "09302015/sch_RCRI_09302015.rds", "12312015/sch_RCRI_12312015.rds", "03312016/sch_RCRI_03312016.rds", "06302016/sch_RCRI_06302016.rds", "09302016/sch_RCRI_09302016.rds", "12312016/sch_RCRI_12312016.rds", "03312017/sch_RCRI_03312017.rds", "06302017/sch_RCRI_06302017.rds",  "09302017/sch_RCRI_09302017.rds", "12312017/sch_RCRI_12312017.rds", "03312018/sch_RCRI_03312018.rds", "06302018/sch_RCRI_06302018.rds",  "09302018/sch_RCRI_09302018.rds", "12312018/sch_RCRI_12312018.rds")
 
     num <- length(reports)
 
